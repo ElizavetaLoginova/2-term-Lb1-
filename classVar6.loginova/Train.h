4af254a0f5aaae429e546c2804e2a5eedda6467f
ElizavetaLoginova <elizyk@mail.ru>
2016-09-22 13:07:51 +0300

    Update Train.h

--- classVar6.loginova/Train.h
+++ classVar6.loginova/Train.h
@@ -1,4 +1,4 @@
-﻿#ifndef Train_h
+#ifndef Train_h
 #define Train_h
 
 const int N = 20;
@@ -9,16 +9,22 @@ class Train
 public:
 	Train();
 	~Train();
+	void SetTrain(char*, char*, char*, char*, char*, char*);
 	void Destination(char*);
 	void TrainNumber(char*);
 	void DepartureTime(char*);
-	void SetTrain(char*, char*, char*);
+	void NumberOfCommonPlaces(char*);
+	void NumOfCompartment(char*);
+	void SecondClasSeats(char*);
 	char* GetcarDestination();
 	char* GetTrainNumber();
 	char* GetdepartureTime();
+	char* GetNumberOfCommonPlaces();
+	char* GetNumOfCompartmen();
+	char* GetSecondClasSeats();
 	void DisplayTrain();
 private:
-	char carDestination[N], trainNumber[N], departureTime[N];
+	char carDestination[N], trainNumber[N], departureTime[N], numCommon[N], numCompartment[N], secondClass[N];
 };
 
 #endif
