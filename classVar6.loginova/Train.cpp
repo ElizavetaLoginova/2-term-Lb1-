88284dbf72e4127949ef55c3af47c9b7c34ff815
ElizavetaLoginova <elizyk@mail.ru>
2016-09-22 13:07:18 +0300

    Update Train.cpp

--- classVar6.loginova/Train.cpp
+++ classVar6.loginova/Train.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string.h>
 #include "Train.h"
 
@@ -12,47 +12,72 @@ void Train::Destination(char* s)
 {
 	strcpy(carDestination, s);
 }
+char* Train::GetcarDestination()
+{
+	return carDestination;
+}
 
 void Train::TrainNumber(char* s)
 {
 	strcpy(trainNumber, s);
+}
+char* Train::GetTrainNumber()
+{
+	return trainNumber;
 }
 
 void Train::DepartureTime(char* s)
 {
 	strcpy(departureTime, s);
 }
-
-
-char* Train::GetcarDestination()
-{
-	return carDestination;
-}
-
-char* Train::GetTrainNumber()
-{
-	return trainNumber;
-}
-
 char* Train::GetdepartureTime()
 {
 	return departureTime;
 }
 
+void Train::NumberOfCommonPlaces(char* s)
+{
+	strcpy(numCommon, s);
+}
+char* Train::GetNumberOfCommonPlaces()
+{
+	return numCommon;
+}
 
+void Train::NumOfCompartment(char* s)
+{
+	strcpy(numCompartment, s);
+}
+char* Train::GetNumOfCompartmen()
+{
+	return numCompartment;
+}
 
-void Train::SetTrain(char* s1, char* s2, char* s3)
+void Train::SecondClasSeats(char* s)
+{
+	strcpy(secondClass, s);
+}
+char* Train::GetSecondClasSeats()
+{
+	return secondClass;
+}
+
+void Train::SetTrain(char* s1, char* s2, char* s3, char* s4, char* s5, char* s6)
 {
 	Destination(s1);
 	TrainNumber(s2);
 	DepartureTime(s3);
-
+	NumberOfCommonPlaces(s4);
+	NumOfCompartment(s5);
+	SecondClasSeats(s6);
 }
 
 void Train::DisplayTrain()
 {
-	cout << endl << "The destination - " << carDestination << " Train number - " << trainNumber << ", \t";
+	cout << endl << "The destination - " << carDestination << ", Train number - " << trainNumber << ", \t";
 	cout.precision(3);
-	cout << "  , \t" << departureTime << ".";
+	cout << "  \t" << departureTime << ", Number of common seats - " << numCommon;
+	cout << ", Number of compartment seats - " << numCompartment << ", The number of second-class seats - " << secondClass;
 
 }
+
